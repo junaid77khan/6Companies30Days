fb13ad6ca4d860af3f9c321cb58e13761eb6da16
Junaid <risklife12@email.com>
2024-01-01 18:46:11 +0530

    Your commit message

---
+++ Number_following_a_pattern/Solution.java
@@ -0,0 +1,36 @@
+package com.Junaid.ReviseWithArsh.Number_following_a_pattern;
+
+import java.util.Stack;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(printMinNumberForPattern("IIDDD"));
+    }
+    static String printMinNumberForPattern(String S){
+        StringBuilder str = new StringBuilder();
+        Stack<Integer> stack = new Stack<Integer>();
+        int curr = 1;
+
+        for(int idx = 0; idx < S.length(); idx++) {
+            if(S.charAt(idx) == 'D') {
+                stack.push(curr);
+                curr++;
+            }
+            else {
+                stack.push(curr);
+                curr++;
+
+                while(stack.size() > 0) {
+                    str.append(stack.pop());
+                }
+            }
+        }
+        stack.push(curr);
+        while(stack.size() > 0) {
+            str.append(stack.pop());
+        }
+
+        return str.toString();
+    }
+}
