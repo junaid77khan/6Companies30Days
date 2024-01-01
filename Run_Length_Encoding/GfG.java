3337c49ac11835331119510e73d90a57501ad77a
Junaid <risklife12@email.com>
2024-01-01 18:46:11 +0530

    Your commit message

---
+++ Run_Length_Encoding/GfG.java
@@ -0,0 +1,26 @@
+package com.Junaid.ReviseWithArsh.Run_Length_Encoding;
+
+public class GfG {
+    public static void main(String[] args) {
+        System.out.println(encode("abbbcdddd"));
+    }
+    public static String encode(String str)
+    {
+        StringBuilder ans = new StringBuilder();
+        int len = 1;
+        for(int idx = 1; idx < str.length(); idx++) {
+            if(str.charAt(idx) == str.charAt(idx-1)) {
+                len++;
+            }
+            else {
+                ans.append(str.charAt(idx-1));
+                ans.append(len);
+                len = 1;
+            }
+        }
+        ans.append(str.charAt(str.length()-1));
+        ans.append(len);
+
+        return ans.toString();
+    }
+}
